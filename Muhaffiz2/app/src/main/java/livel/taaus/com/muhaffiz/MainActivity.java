778531ea963004dadae637b54e9df48c6eaf1755
abdullah345 <abdullahamir9@gmail.com>
2018-11-25 01:38:58 +0500

    app of muhaffiz project

---
+++ Muhaffiz2/app/src/main/java/livel/taaus/com/muhaffiz/MainActivity.java
@@ -0,0 +1,32 @@
+package livel.taaus.com.muhaffiz;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+
+public class MainActivity extends AppCompatActivity {
+    int signincheck=0;
+    LinearLayout hist;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        hist=(LinearLayout)findViewById(R.id.histLinear);
+        hist.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this,History.class);
+                startActivity(intent);
+            }
+        });
+        
+        if (signincheck==0)
+        {
+            //Intent i =new Intent(MainActivity.this,Login.class);
+            //startActivity(i);
+        }
+
+    }
+}
